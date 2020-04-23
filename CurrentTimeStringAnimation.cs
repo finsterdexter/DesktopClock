7cc5c62bfd3eebaa2f9561b404a257a9ea134512
George Walker <finster@gmail.com>
2020-04-22 20:33:00 -0500

    mvp

---
+++ CurrentTimeStringAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace DesktopClock
+{
+	public class CurrentTimeStringAnimation : StringAnimationBase
+	{
+		protected override Freezable CreateInstanceCore()
+		{
+			return new CurrentTimeStringAnimation();
+		}
+
+		protected override string GetCurrentValueCore(string defaultOriginValue, string defaultDestinationValue, AnimationClock animationClock)
+		{
+			return DateTime.Now.ToLongTimeString();
+		}
+	}
+}
