6792ac8392c9a45b0d24681a3ba60fd2534b15b5
George Walker <gwalker@coinlion.com>
2022-01-04 08:06:55 -0600

    Add position/size settings

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,5 +25,40 @@ namespace DesktopClock
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+			if (WindowState == WindowState.Maximized)
+			{
+				// Use the RestoreBounds as the current values will be 0, 0 and the size of the screen
+				Properties.Settings.Default.Top = RestoreBounds.Top;
+				Properties.Settings.Default.Left = RestoreBounds.Left;
+				Properties.Settings.Default.Height = RestoreBounds.Height;
+				Properties.Settings.Default.Width = RestoreBounds.Width;
+				Properties.Settings.Default.Maximized = true;
+			}
+			else
+			{
+				Properties.Settings.Default.Top = this.Top;
+				Properties.Settings.Default.Left = this.Left;
+				Properties.Settings.Default.Height = this.Height;
+				Properties.Settings.Default.Width = this.Width;
+				Properties.Settings.Default.Maximized = false;
+			}
+			Properties.Settings.Default.Save();
+        }
+
+        private void Window_SourceInitialized(object sender, EventArgs e)
+        {
+			this.Top = Properties.Settings.Default.Top;
+			this.Left = Properties.Settings.Default.Left;
+			this.Height = Properties.Settings.Default.Height;
+			this.Width = Properties.Settings.Default.Width;
+			// Very quick and dirty - but it does the job
+			if (Properties.Settings.Default.Maximized)
+			{
+				WindowState = WindowState.Maximized;
+			}
+		}
+    }
 }
